86f8e684eda98120e337eff0c479203cbd6c954c
kelter <miguel.aka.kelter@gmail.com>
2014-12-15 15:10:36 +0000

    single - twitter

--- single.php
+++ single.php
@@ -29,72 +29,73 @@ global $the_post_id;
 
 <div class="container">
 	<?php if(have_posts()) : ?>
-	<?php while(have_posts()) : the_post(); ?>
-	<?php 
+		<?php while(have_posts()) : the_post(); ?>
+			<?php 
 						// data for side bar
-	$the_post_id = get_the_ID();
-	$the_author_name = get_the_author_meta('display_name');
-	$the_author_description = get_the_author_meta('description');
-	$the_author_id = get_the_author_meta('ID');
-	$allcategories = get_the_category($the_post_id);
-	if( $allcategories )
-		$category = $allcategories[0];
-	if( $category )
-		$the_category = $category->cat_name;
-	?>
-
-	<?php if ( is_mobile() == false ) {?>
-	<div class="category">
-		<a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a> › 
-		<?php if( $category ) : ?>
-		<a href="<?php echo ($category ? get_category_link( $category->term_id ) : '#'); ?>"><?php echo $the_category ?></a>
-	<?php endif; ?>
-</div>
-
-<?php 
-}
-?>
-
-<div class="row">
-	<div class="leftbar span8">
-		<div class="fullpost">
-			<h2><?php the_title(); ?></h2>
-			<p class="postmetadata byline">
-				By <?php  the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
-			</p>
-			<div class="postcontent"><?php the_content(); ?></div>
-			<?php
-			echo get_the_tag_list('<div class="tags"><i></i> ','','</div>');
+			$the_post_id = get_the_ID();
+			$the_author_name = get_the_author_meta('display_name');
+			$the_author_description = get_the_author_meta('description');
+			$the_author_id = get_the_author_meta('ID');
+			$allcategories = get_the_category($the_post_id);
+			if( $allcategories )
+				$category = $allcategories[0];
+			if( $category )
+				$the_category = $category->cat_name;
 			?>
 
-			<div class="author_info">
-				<div class="author_photo">
-					<style>
-					.author_photo { 
-						background-image	: url("<?php $userdata = get_userdata($the_author_id); $updir = wp_upload_dir(); echo $updir['baseurl'] . '/userphoto/' . $userdata->userphoto_thumb_file; ?>"); 
-					}
-					</style>
-				</div>
-				<div class="author_data">
-					<div class="author_about">About the author</div>
-					<div class="author_name"><h4><?php echo $the_author_name; ?></h4></div>
-					<div class="author_bio"><?php echo $the_author_description; ?></div>
-					<?php if ( get_the_author_meta( 'twitter' ) ) { ?>
-						<p class="twitter clear">
-							[twitter-follow screen_name='<?php the_author_meta( 'twitter' ); ?>']
-						</p>
-					<?php } // End check for twitter ?>
+			<?php if ( is_mobile() == false ) {?>
+			<div class="category">
+				<a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a> › 
+				<?php if( $category ) : ?>
+					<a href="<?php echo ($category ? get_category_link( $category->term_id ) : '#'); ?>"><?php echo $the_category ?></a>
+				<?php endif; ?>
 			</div>
-			<div style="clear:both"></div>
-		</div>
-	</div>
-	<div class="comments-template">
-		<?php comments_template('/comments.php'); ?>
-	</div>				
-</div>
-<?php endwhile; ?>	
-<?php get_sidebar(); ?>	
-<?php endif; ?>
+
+			<?php 
+		}
+		?>
+
+		<div class="row">
+			<div class="leftbar span8">
+				<div class="fullpost">
+					<h2><?php the_title(); ?></h2>
+					<p class="postmetadata byline">
+						By <?php  the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+					</p>
+					<div class="postcontent"><?php the_content(); ?></div>
+					<?php
+					echo get_the_tag_list('<div class="tags"><i></i> ','','</div>');
+					?>
+
+					<div class="author_info">
+						<div class="author_photo">
+							<style>
+								.author_photo { 
+									background-image	: url("<?php $userdata = get_userdata($the_author_id); $updir = wp_upload_dir(); echo $updir['baseurl'] . '/userphoto/' . $userdata->userphoto_thumb_file; ?>"); 
+								}
+							</style>
+						</div>
+						<div class="author_data">
+							<div class="author_about">About the author</div>
+							<div class="author_name"><h4><?php echo $the_author_name; ?></h4></div>
+							<div class="author_bio"><?php echo $the_author_description; ?></div>
+							<?php if ( get_the_author_meta( 'twitter' ) ) { ?>
+							<p class="twitter clear" style="margin-top: 10px;">
+								<a href="https://twitter.com/<?php the_author_meta( 'twitter' ); ?>" class="twitter-follow-button" data-show-count="false">Follow @<?php the_author_meta( 'twitter' ); ?></a>
+								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+							</p>
+							<?php } // End check for twitter ?>
+						</div>
+						<div style="clear:both"></div>
+					</div>
+				</div>
+				<div class="comments-template">
+					<?php comments_template('/comments.php'); ?>
+				</div>				
+			</div>
+		<?php endwhile; ?>	
+		<?php get_sidebar(); ?>	
+	<?php endif; ?>
 </div>
 </div>
 
