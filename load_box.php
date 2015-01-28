d6e31fd111a46987b4fdab03560f1f437dcfb371
Mykola Tkachenko <piranya87@gmail.com>
2015-01-28 11:36:22 +0000

    search fix

--- load_box.php
+++ load_box.php
@@ -1,5 +1,8 @@
+<?php if ($_REQUEST["s"]=="") :?>
 <div class="LoadBox">
 	<div id="LoadMoreDiv" ><a href="javascript:load_posts();"><?php _e("Load more...","outsystems_blog");?></a></div>
 	<div id="LoadingDiv" style="display: none;"><?php _e("Loading","outsystems_blog");?></div>
 	<div id="LoadingError"  style="display: none;"><?php _e("Error loading more posts.","outsystems_blog");?></div>
 </div>
+<?php endif; ?>
+
