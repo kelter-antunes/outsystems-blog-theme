82bdc61887ba2d1a9249fa7081761bba19b0465b
Mykola Tkachenko <piranya87@gmail.com>
2015-02-03 17:20:45 +0000

    Load "subscription form" from plugin options.

--- subscription_area.php
+++ subscription_area.php
@@ -5,9 +5,8 @@
 			if( $success != "" )
                 echo '<div class="subscribed">'.__("Thank you for subscribing to our blog!","outsystems_blog").'</div>';
             else {
-                echo '<script src="//app-sj03.marketo.com/js/forms2/js/forms2.js"></script>
-				<form id="mktoForm_1119"></form>
-				<script>MktoForms2.loadForm("//app-sj03.marketo.com", "338-PNW-019", 1119);</script>';
+				echo stripslashes(get_option('subscription_form'));
             }
 			?>
 </div>
+
